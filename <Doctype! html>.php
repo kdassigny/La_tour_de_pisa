86892cd6883ef3da498c83abcd71cc63aa05ca2f
dassigny <dassigny.kevin@hotmail.fr>
2016-04-20 08:50:32 +0200

    ajout dossier tour

---
+++ <Doctype! html>.php
@@ -0,0 +1,34 @@
+<Doctype! html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>Index_la_tour_de_Pise</title>
+  <link rel="stylesheet" href="style.css">
+  <script src="script.js"></script>
+</head>
+<body>
+   <header>
+   		<div>  <!--banière-->
+   			<h1>La Tour de Pise</h1>
+   		</div>
+   		<div>  <!-- sommaire-->
+   			<table width="450px">
+			<tr style="background-color:teal;color:white;">
+				<th>La carte</th>
+				<th>Commande</th>                  
+			</tr>
+   		</div>
+   		<div>  <!--slider-->
+   			
+   		</div>
+   		<div>  <!--photo stefano-->
+   			
+   		</div>  
+   		<div>  <!--description-->
+   			
+   		</div>
+   </header>
+   <footer>
+   </footer>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,35 @@
+<!Doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>Index_la_tour_de_Pise</title>
+  <link rel="stylesheet" href="style.css">
+  <script src="script.js"></script>
+</head>
+<body>
+   <header>
+   		<div>  <!--banière-->
+   			<h1>La Tour de Pise</h1>
+   		</div>
+   		<div>  <!-- sommaire-->
+   			<table width="450px">
+			<tr style="background-color:green;color:white;">
+				<th href="file://var/www/html/la_tour_de_pisa/index.html">Acceuil</th>
+				<th>La carte</th>
+				<th>Commande</th>                  
+			</tr>
+   		</div>
+   		<div>  <!--slider-->
+   			
+   		</div>
+   		<div>  <!--photo stefano-->
+   			
+   		</div>  
+   		<div>  <!--description-->
+   			
+   		</div>
+   </header>
+   <footer>
+   </footer>
+</body>
+</html>
